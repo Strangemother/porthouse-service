9a06b5f0c97d65675f935894a33f6922eeecbd23
Jay Jagpal <jay@strangemother.com>
2024-01-07 03:22:55 +0000

    Apply account json view and some notes

--- website/primary/tokens/urls.py
+++ website/primary/tokens/urls.py
@@ -7,16 +7,22 @@ from . import views
 app_name = 'tokens'
 
 urlpatterns = t_urls.paths_named(views,
+    ## Frontend user access
     list=('TokenListView', ('', 'list/',),),
     create=('TokenCreateView', 'create/'),
 
     ## API urls.
+    ### Token info
     ask=('TokensizerAskFormView', 'ask/'),
     info=('TokenInfoFormView', 'info/'),
     exists=('TokenExistsView', 'exists/'),
+    ### User info
+    user_detail=('AccountTokenDetailView', 'user/'),
 
+    ## Frontend user access
     detail=('TokenDetailView', 'token/<str:slug>/'),
     delete=('TokenDeleteView', 'token/<str:slug>/delete/'),
     subscribe=('TokenSubscribeView', 'token/<str:slug>/subscribe/'),
+
 )
 
